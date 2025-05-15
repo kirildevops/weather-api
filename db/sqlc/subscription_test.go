fb317d6b1899bb838055b1073ec9c515d0af811d
kirildevops <211359927+kirildevops@users.noreply.github.com>
2025-05-15 14:15:10 +0300

    finalize tests

--- db/sqlc/subscription_test.go
+++ db/sqlc/subscription_test.go
@@ -22,6 +22,7 @@ func createRandomSubscription(t *testing.T) Subscription {
 	require.Equal(t, arg.Email, sub.Email)
 	require.Equal(t, arg.City, sub.City)
 	require.Equal(t, arg.Frequency, sub.Frequency)
+	require.Equal(t, sub.Confirmed, false)
 
 	require.NotZero(t, sub.ID)
 	require.NotZero(t, sub.Token)
@@ -41,6 +42,34 @@ func TestGetSubscription(t *testing.T) {
 
 	require.Equal(t, sub1.ID, sub2.ID)
 	require.Equal(t, sub1.City, sub2.City)
+	require.Equal(t, sub1.Frequency, sub2.Frequency)
 	require.Equal(t, sub1.Token, sub2.Token)
 	require.Equal(t, sub1.Confirmed, sub2.Confirmed)
+}
+
+func TestConfirmSubscription(t *testing.T) {
+	sub_before := createRandomSubscription(t)
+	err := testQueries.ConfirmSubscription(context.Background(), sub_before.Token)
+	require.NoError(t, err)
+	sub_after, err := testQueries.GetSubscription(context.Background(), sub_before.Email)
+	require.Equal(t, sub_after.Confirmed, true)
+	require.Equal(t, sub_after.ID, sub_before.ID)
+	require.Equal(t, sub_after.Email, sub_before.Email)
+	require.Equal(t, sub_after.City, sub_before.City)
+	require.Equal(t, sub_after.Frequency, sub_before.Frequency)
+	require.Equal(t, sub_after.Token, sub_before.Token)
+
+}
+
+func TestDeleteSubscription(t *testing.T) {
+	deleteme := createRandomSubscription(t)
+	arg := DeleteSubscriptionParams{
+		Email: deleteme.Email,
+		Token: deleteme.Token,
+	}
+	testQueries.DeleteSubscription(context.Background(), arg)
+
+	nobody, err := testQueries.GetSubscription(context.Background(), deleteme.Email)
+	require.Errorf(t, err, "sql: no rows in result set")
+	require.Empty(t, nobody)
 }
