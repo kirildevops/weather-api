7080b6d1c882d7365c40abad4fa6e25b7980ea70
kirildevops <211359927+kirildevops@users.noreply.github.com>
2025-05-17 14:56:29 +0300

    Finalize weather and subscribe endpoints

--- api/subscription.go
+++ api/subscription.go
@@ -3,12 +3,15 @@ package api
 import (
 	"database/sql"
 	"errors"
+	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 	db "github.com/kirildevops/weather-api/db/sqlc"
+	"github.com/lib/pq"
 )
 
 type CreateSubscriptionRequest struct {
@@ -20,8 +23,12 @@ type CreateSubscriptionRequest struct {
 func (server *Server) subscribe(ctx *gin.Context) {
 	var req CreateSubscriptionRequest
 	if err := ctx.ShouldBindJSON(&req); err != nil {
-		ctx.JSON(http.StatusBadRequest, errorResponse(err))
-		return
+		if validationErrors, ok := err.(validator.ValidationErrors); ok {
+			ctx.JSON(http.StatusBadRequest, errorResponse(errors.New("Invalid input")))
+			fmt.Println(validationErrors)
+			return
+		}
+
 	}
 
 	arg := db.InsertSubscriptionParams{
@@ -32,10 +39,42 @@ func (server *Server) subscribe(ctx *gin.Context) {
 
 	subscription, err := server.store.InsertSubscription(ctx, arg)
 	if err != nil {
+		if err, ok := err.(*pq.Error); ok {
+			// duplicate key value violates unique constraint
+			if err.Code == "23505" {
+				ctx.JSON(http.StatusConflict, errorResponse(errors.New("Email already subscribed")))
+				return
+			}
+			// fmt.Println("Severity:", err.Severity)
+			// fmt.Println("Code:", err.Code)
+			// fmt.Println("Message:", err.Message)
+			// fmt.Println("Detail:", err.Detail)
+			// fmt.Println("Schema:", err.Schema)
+			// fmt.Println("Table:", err.Table)
+			// fmt.Println("Constraint:", err.Constraint)
+			// fmt.Println("File:", err.File)
+			// fmt.Println("Line:", err.Line)
+			// fmt.Println("Routine:", err.Routine)
+			//
+			// Produces
+			//
+			// Severity: ERROR
+			// Code: 23505
+			// Message: duplicate key value violates unique constraint "subscriptions_email_key"
+			// Detail: Key (email)=(your@email.com) already exists.
+			// Schema: public
+			// Table: subscriptions
+			// Constraint: subscriptions_email_key
+			// File: nbtinsert.c
+			// Line: 563
+			// Routine: _bt_check_unique
+		}
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
 		return
 	}
-	ctx.JSON(http.StatusOK, subscription)
+	// ctx.JSON(http.StatusOK, subscription)
+	fmt.Println(subscription)
+	ctx.JSON(http.StatusOK, "Subscription successful. Confirmation email sent.")
 
 }
 
